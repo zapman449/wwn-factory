ed8a2e1cca724d619c05c0710da5f5236d227b61
Jason Price <jprice@eponine.(none)>
2012-06-24 09:51:55 -0400

    better command line parsing for wwn_remote.sh

--- wwn_remote.sh
+++ wwn_remote.sh
@@ -2,18 +2,36 @@
 
 URL='http://localhost:8080/wwn_fabric'
 
-FQDN='repdb3b00.be.weather.com'
+#FQDN='repdb3b00.be.weather.com'
 
 SYSPATH='/tmp/sys/class/fc_host/'
 #SYSPATH='/sys/class/fc_host/'
 
+if [ "$1" = "create" ] || [ "$1" = "CREATE" ] ; then
+    VPORT="vport_create"
+elif [ "$1" = "destroy" ] || [ "$1" = "DESTROY" ] || [ "$1" = "delete"] || [ "$1" = "DELETE" ] ; then
+    VPORT="vport_delete"
+else 
+    echo "USAGE: $0 <create|delete> FQDN"
+    exit
+fi
+
+shift
+
+if [ -z "$1" ]; then
+    echo "USAGE: $0 <create|delete> FQDN"
+    exit
+else
+    FQDN=$1
+fi
+
 for fchost in ${SYSPATH}/host* ; do
-    if [ -f ${fchost}/vport_create ]; then
+    if [ -f ${fchost}/${VPORT} ]; then
         fabric=`cat ${fchost}/fabric_name | sed "s/^0x//"`
         #echo "getting ${URL}/${FQDN}/${fabric}"
         result=`curl --silent ${URL}/${FQDN}/${fabric}`
         #echo " -- FQDN: $FQDN -- Fabric ${fabric} -- Result ${result} -- "
         #echo "-- $fabric --"
-        echo "echo \"$result\" > ${fchost}/vport_create"
+        echo "echo \"$result\" > ${fchost}/${VPORT}"
     fi
 done
